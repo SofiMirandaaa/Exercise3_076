d19f5d3e3f98021c1d67275ffcbb2a271f85505f
Sofi Miranda <sofi.miranda.ft21@mail.umy.ac.id>
2022-11-25 17:26:41 +0700

    add search for add and delete

--- Exercise3_076/Program.cs
+++ Exercise3_076/Program.cs
@@ -153,17 +153,19 @@ namespace Exercise3_076
                 try
                 {
                     Console.WriteLine("\nMenu");
-                    Console.WriteLine("1. View all the records in the list");
-                    Console.WriteLine("2. Search for a record in the list");
-                    Console.WriteLine("3. Display the first record in the list");
-                    Console.WriteLine("4. Exit");
+                    Console.WriteLine("1. Add a record to the list");
+                    Console.WriteLine("2. Delete a record from the list");
+                    Console.WriteLine("3. View all the records in the list");
+                    Console.WriteLine("4. Search for a record in the list");
+                    Console.WriteLine("5. Display the first record in the list");
+                    Console.WriteLine("6. Exit");
                     Console.Write("\nEnter your choice (1-4): ");
                     char ch = Convert.ToChar(Console.ReadLine());
                     switch (ch)
                     {
                         case '1':
                             {
-                                obj.traverse();
+                                obj.addNode();
                             }
                             break;
                         case '2':
@@ -189,10 +191,15 @@ namespace Exercise3_076
                             break;
                         case '3':
                             {
+                                obj.traverse();
+                            }
+                            break;
+                        case '5':
+                            {
                                 obj.firstNode();
                             }
                             break;
-                        case '4':
+                        case '6':
                             return;
                         default:
                             {
