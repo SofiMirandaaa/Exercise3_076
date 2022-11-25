e30c84f1d57453d4bef708c17b01739a9a505ebe
Sofi Miranda <sofi.miranda.ft21@mail.umy.ac.id>
2022-11-25 17:01:56 +0700

    add class CircularList and void addnode

--- Exercise3_076/Program.cs
+++ Exercise3_076/Program.cs
@@ -14,6 +14,25 @@ namespace Exercise3_076
         public string name;
         public Node next;
     }
-   
+    class CircularList
+    {
+        Node LAST;
 
+        public CircularList()
+        {
+            LAST = null;
+        }
+        public void addNode()
+        {
+            int nim;
+            string nm;
+            Console.WriteLine("\nEnter the roll number of the student: ");
+            nim = Convert.ToInt32(Console.ReadLine());
+            Console.Write("\nEnter the name of the student: ");
+            nm = Console.ReadLine();
+            Node newNode = new Node();
+            newNode.studentnum = nim;
+            newNode.name = nm;
+        }
+    }
 }
