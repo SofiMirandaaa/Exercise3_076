44e013220566423bc1d3d9454199bcc1f75996dc
Sofi Miranda <sofi.miranda.ft21@mail.umy.ac.id>
2022-11-25 17:12:54 +0700

    add public bool search

--- Exercise3_076/Program.cs
+++ Exercise3_076/Program.cs
@@ -33,6 +33,61 @@ namespace Exercise3_076
             Node newNode = new Node();
             newNode.studentnum = nim;
             newNode.name = nm;
+
+            //check if the list empty
+            if (LAST == null || nim <= LAST.studentnum)
+            {
+                if ((LAST != null) && (nim == LAST.studentnum))
+                {
+                    Console.WriteLine("\nDuplicate number not allowed");
+                    return;
+                }
+                newNode.next = LAST;
+                if (LAST != null)
+                    LAST.next = newNode;
+                newNode.next = null;
+                LAST = newNode;
+                return;
+            }
+            /*if the node is to be inserted at between two node*/
+            Node previous, current;
+            for (current = previous = LAST;
+                 current != null && nim >= current.studentnum;
+                 previous = current, current = current.next)
+            {
+                if (nim == current.studentnum)
+                {
+                    Console.WriteLine("\nDuplicate numbers not allowed");
+                }
+            }
+            /* On the execution of the above for loop, prev and
+             * current will point to those nodes
+             * between which the new nod is to be inserted */
+            newNode.next = current;
+
+            //if the node is to be inserted at the end of the list 
+            if (current == null)
+            {
+                newNode.next = null;
+                previous.next = newNode;
+                return;
+            }
+            previous.next = newNode;
         }
+        public bool Search(int rollNo, ref Node previous, ref Node current)
+        /*searches for the specified node*/
+        {
+            for (previous = current = LAST.next; current != LAST; previous = current, current = current.next)
+            {
+                if (rollNo == current.rollNumber)
+                    return (true); /*returns true if the node is found*/
+            }
+            if (rollNo == LAST.rollNumber)
+                return true;
+            else
+                return (false); /*returns false if the node is not found*/
+        }
+
     }
+    
 }
