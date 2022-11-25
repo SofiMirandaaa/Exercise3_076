51fe6e4f6995d0797fd1cde4f39ee9aa12323bfe
Sofi Miranda <sofi.miranda.ft21@mail.umy.ac.id>
2022-11-25 16:14:34 +0700

    add class Node

--- Exercise3_076/Program.cs
+++ Exercise3_076/Program.cs
@@ -6,6 +6,14 @@ using System.Threading.Tasks;
 
 namespace Exercise3_076
 {
+    class Node
+    {
+        /*create Nodes for the ciscular nexted list*/
+        public int rollNumber;
+        public string name;
+        public Node next;
+    }
+
     internal class Program
     {
         static void Main(string[] args)
